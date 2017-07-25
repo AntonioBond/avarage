21ad20fae900e420bd42b41f9c7b7d38a4f0940b
AntonioBond <antonio_bond1993@mail.ru>
2017-07-25 18:06:50 +0300

    Добавлены вычесления среднего арифметического и среднего геометрического

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello World')
+# Программа для подсчета средней арифметической и средней геометрической
+
+x = list(input("Введите числа").split(' '))
+list_of_int = []
+for i in x:
+    list_of_int.append(int(i))
+
+
+averageArifm = sum(list_of_int) / len(list_of_int)
+print(averageArifm)
+
+multi = 1
+for i in list_of_int:
+    multi *= i
+
+averageGeom = multi ** (1 / len(list_of_int))
+print(averageGeom)
