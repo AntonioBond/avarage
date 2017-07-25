d6bcd91704bfbf139578818b02610abb454c65ca
AntonioBond <antonio_bond1993@mail.ru>
2017-07-25 18:19:29 +0300

    Создали функции вычисления и вызова справки

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 # Программа для подсчета средней арифметической и средней геометрической
 
 x = list(input("Введите числа").split(' '))
@@ -6,12 +9,21 @@ for i in x:
     list_of_int.append(int(i))
 
 
-averageArifm = sum(list_of_int) / len(list_of_int)
-print(averageArifm)
+def helps():
+    print("Используйте -aA для вычесления средней арифметической")
+    print("Используйте -aG для вычесления средней арифметической")
+    print("Используйте -help для вызова справки")
 
-multi = 1
-for i in list_of_int:
-    multi *= i
 
-averageGeom = multi ** (1 / len(list_of_int))
-print(averageGeom)
+def arithmetic():
+    average_arithm = sum(list_of_int) / len(list_of_int)
+    return average_arithm
+
+
+def geometric():
+    multi = 1
+    for i in list_of_int:
+        multi *= i
+    average_geom = multi ** (1 / len(list_of_int))
+    return average_geom
+
